cf2db08d69c9df15bcf5b7c6f89862dbdd901059
Zack Klinger <zklinger@gmail.com>
2016-04-20 20:15:56 -0700

    ADD logic to check for a winning board

--- app.js
+++ app.js
@@ -14,7 +14,40 @@ angular.module('ticTacToeApp', [])
   
 })
 .service('TicTacToe', function() {
+  this._playerToken = '';
+  this.turnToken = 'X';
   this.gameBoard = [];
+  this.winState = false;
+  
+  // Check for win
+  function checkResults(board, token) {
+    var win = false;
+    var rows = _.zip.apply(this, board);
+    // Check if any row contains all the same token
+    for (var j = 0; j < 3; j++) {
+      if (!win) {
+        win = rows[j].every(function(square) {
+          return square.token === token;
+        });
+      }
+    }
+    // Check if any column contains all the same token
+    for (var i = 0; i < 3; i++) {
+      if (!win) {
+        win = board[i].every(function(square) {
+          return square.token === token;
+        });
+      }
+    }
+    // Check for diagnol wins
+    if (!win) {
+      win = board[0][0].token === token && board[1][1].token === token && board[2][2].token === token;
+    }
+    if (!win) {
+      win = board[0][2].token === token && board[1][1].token === token && board[2][0].token === token;
+    }
+    return win;
+  }
   
   // Reset method
   this.reset = function() {
@@ -23,14 +56,46 @@ angular.module('ticTacToeApp', [])
       newBoard[i] = new Array(3);
       for (var j = 0; j < 3; j++) {
         newBoard[i][j] = {
-          row: i,
-          col: j,
+          col: i,
+          row: j,
           token: ''
         };
       }
     }
     this.gameBoard = newBoard;
     console.log(this.gameBoard);
+  }
+  // Set Player token
+  this.setPlayerToken = function(token) {
+    console.log(token);
+    this._playerToken = token || 'X';
+    // Set computer to opposite token
+    this._computerToken = this._playerToken === 'X' ? 'O' : 'X';
+    console.log(this._computerToken);
+  }
+  // Click listener for picking a square
+  this.pickSquare = function(square) {
+    var row = square.row;
+    var col = square.col;
+    var token = square.token;
+    // Check for empty player token
+    if (this._playerToken === '') {
+      console.error('no player token set!');
+      return;
+    }
+    // Check if square is empty
+    if (token === '') {
+      console.info('it is EMPTY!');
+      // Set token
+      square.token = this._playerToken;
+    } else {
+      console.log('it is has a "' + square.token + '" there');
+      return;
+    }
+    // Check for win
+    this.winState = checkResults(this.gameBoard, this._playerToken);
+    console.log(this.winState);
+    // if it's a win, add to score of turn token
   }
   
   this.reset();
